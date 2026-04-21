956944572a79b9b0a481b73dd6dcb774dc62b1ad
mmazanti <math.mazanti@gmail.com>
2026-04-21 18:09:34 -0300

    Create Desafio005.py
    Resolução do desafio005: #Faça um programa que leia algo pelo teclado e mostre na tela seu tipo primitivo e todas as informações possíveis sobre ele.

--- Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio005.py
+++ Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio005.py
@@ -1,1 +1,7 @@
 #Faça um programa que leia algo pelo teclado e mostre na tela seu tipo primitivo e todas as informações possíveis sobre ele.
+escrita = input('Digite algo: ')
+print(type(escrita))
+print('{} é um número?'.format(escrita), escrita.isalnum())
+print('{} é uma letra?'.format(escrita), escrita.isalpha())
+print('{} está em maiúsculo?'.format(escrita), escrita.isupper())
+print('{} está em minúsculo?', escrita.islower())
