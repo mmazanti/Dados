87ce024c2f06b3325a3525d0a5bb0a05637bac04
mmazanti <math.mazanti@gmail.com>
2026-04-21 16:34:32 -0300

    Create Desafio#03.py
    Resolução desafio 3: Crie um script python que leia dois números e tente mostrar a soma entre eles.

---
+++ Python/Curso_em_video-Mundo 1/Desafio#03.py
@@ -0,0 +1,6 @@
+#Desafio 3: Crie um script python que leia dois números e tente mostrar a soma entre eles.#
+print('====== DESAFIO 03 ======')
+numero_1= int(input('Primeiro número: '))
+numero_2= int(input('Segundo número: '))
+soma= numero_1 + numero_2
+print('A soma é', soma)
