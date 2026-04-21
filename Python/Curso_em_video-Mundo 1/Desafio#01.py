eb863588ddf5d288b46a4b446017e9241fd97098
mmazanti <math.mazanti@gmail.com>
2026-04-21 16:20:45 -0300

    Create Desafio#01.py
    Resolução do desafio#01. Criar um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.

---
+++ Python/Curso_em_video-Mundo 1/Desafio#01.py
@@ -0,0 +1,4 @@
+#Desafio 1: Crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado#
+print('====== DESAFIO 01 ======')
+nome= input('Digite seu nome: ')
+print('Bem vindo, ', nome, '! Prazer em te conhecer')
