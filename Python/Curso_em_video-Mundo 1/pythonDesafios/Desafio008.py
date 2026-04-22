37177dac588f8b431708bc5f09942ba27c066316
mmazanti <math.mazanti@gmail.com>
2026-04-22 13:28:43 -0300

    Create Desafio008.py
    Resolução do desafio 008: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.

---
+++ Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio008.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+nota1 = float(input('Digite o valor da primeira nota: '))
+nota2 = float(input('Digite o valor da segunda nota: '))
+media = (nota1+nota2)/2
+print('A média do aluno é de: {}'.format(media))
