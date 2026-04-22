c5e1eb2f9d8be154109a21a61e2fd752fd051198
mmazanti <math.mazanti@gmail.com>
2026-04-22 13:25:10 -0300

    Create Desafio007.py
    Resolução do desafio 007: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

---
+++ Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio007.py
@@ -0,0 +1,6 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+numero = int(input('Digite um número: '))
+dobro = numero*2
+triplo = numero*3
+raiz_quadrada = numero**(1/2)
+print('O dobro de {} é {}, seu triplo é {} e a raiz quadrada de {} é {}'.format(numero, dobro, triplo, numero, raiz_quadrada))
