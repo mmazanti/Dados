c3c72836b0286a036140346f29feae7117db70d4
mmazanti <math.mazanti@gmail.com>
2026-04-23 15:15:13 -0300

    Create ex007.py
    Resolução do exercício 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.

--- Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio008.py
+++ Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio008.py
@@ -2,4 +2,4 @@
 nota1 = float(input('Digite o valor da primeira nota: '))
 nota2 = float(input('Digite o valor da segunda nota: '))
 media = (nota1+nota2)/2
-print('A média do aluno é de: {}'.format(media))
+print('A média do aluno é de: {}'.format(mdia))
