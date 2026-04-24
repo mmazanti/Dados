bab8a9b5faee2966c35159d8ef47eab30bdaa2db
mmazanti <math.mazanti@gmail.com>
2026-04-24 18:16:57 -0300

    Create ex012.py
    Resolução do exercício 012: Faça um algoritmo que leia o preço de um produto e mostre sue novo preço com 5% de desconto.

---
+++ Python/Curso_em_video-Mundo 1/pythonExercicios/ex012.py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que leia o preço de um produto e mostre sue novo preço com 5% de desconto.
+preço = float(input('Qual é o preço do produto? R$'))
+preço_desconto = preço - (preço*5/100)
+print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai custar R${:.2f}.'.format(preço, preço_desconto))
