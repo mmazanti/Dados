917882a4d0ada0d18456fe590b735ab1150a5aaf
mmazanti <math.mazanti@gmail.com>
2026-04-28 12:50:47 -0300

    Create ex019.py
    Resolução do exercício 019: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele lendo o nome deles e escrevendo o nome do escolhido.

---
+++ Python/Curso_em_video-Mundo 1/pythonExercicios/ex019.py
@@ -0,0 +1,9 @@
+#Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele lendo o nome deles e escrevendo o nome do escolhido.
+from random import choice
+nome1 = str(input('Primeiro aluno: '))
+nome2 = str(input('Segundo aluno: '))
+nome3 = str(input('Terceiro aluno: '))
+nome4 = str(input('Quarto aluno: '))
+lista_aluno = [nome1, nome2, nome3, nome4]
+escolhido = choice(lista_aluno)
+print('O aluno escolhido foi {}.'.format(escolhido))
