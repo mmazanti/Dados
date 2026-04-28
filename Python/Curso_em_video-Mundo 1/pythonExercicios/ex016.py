9a7113312916eaa12f0ec5cf4a972d27c37a4416
mmazanti <math.mazanti@gmail.com>
2026-04-28 12:09:17 -0300

    Create ex016.py
    Resolução do exercício 016: Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.

---
+++ Python/Curso_em_video-Mundo 1/pythonExercicios/ex016.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+'''from math import trunc
+número = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}.'.format(número, trunc(número)))'''
+
+num = float(input('Digite um valor: '))
+print('O valor digitado é {} e a sua porção inteira é {}.'.format(num, int(num)))
