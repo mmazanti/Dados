eab1408ba53a6f7f6298556aa4990b403c6560cb
mmazanti <math.mazanti@gmail.com>
2026-04-29 11:39:39 -0300

    Create Desafio 023.py
    Resolução do desafio 024: Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo".

---
+++ Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio 023.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
+cidade = input('Digite o nome da cidade: ')
+print('Santo' in cidade)
+print("A cidade de {} não tem 'Santo' no seu nome".format(cidade))
