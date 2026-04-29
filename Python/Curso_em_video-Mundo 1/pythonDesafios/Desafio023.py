1487637439d772276e9d882cb134e114749cd194
mmazanti <math.mazanti@gmail.com>
2026-04-29 11:43:43 -0300

    Update Desafio023.py
    Corrigido o desafio023: Ajustado para buscar a string "Santo" apenas no começo da string.

--- Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio023.py
+++ Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio023.py
@@ -1,4 +1,3 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
 cidade = input('Digite o nome da cidade: ')
-print('Santo' in cidade)
-print("A cidade de {} não tem 'Santo' no seu nome".format(cidade))
+print('Santo' in cidade.split()[0])
