c351567d21bc7d9512c228205a67b1ab6c3f56b0
mmazanti <math.mazanti@gmail.com>
2026-04-29 12:08:00 -0300

    Update ex015.py
    Excluído caracter inválido.

--- Python/Curso_em_video-Mundo 1/pythonExercicios/ex015.py
+++ Python/Curso_em_video-Mundo 1/pythonExercicios/ex015.py
@@ -2,5 +2,5 @@
 #Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
 dias = int(input('Quantos dias alugados? '))
 km = float(input('Quantos Km rodados? '))
-pago = (dias * 60) + (km * 0.15)3
+pago = (dias * 60) + (km * 0.15)
 print('O total a pagar é de R${:.2f}'.format(pago))
