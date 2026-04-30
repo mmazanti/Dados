c86ef7c7ddf78536ed62c9fc6a6c400922a4a298
mmazanti <math.mazanti@gmail.com>
2026-04-30 10:40:25 -0300

    Create ex023.py
    Resolução do exercício 023: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.

---
+++ Python/Curso_em_video-Mundo 1/pythonExercicios/ex023.py
@@ -0,0 +1,11 @@
+#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+número = int(input('Informe um número: '))
+unidade = número // 1 % 10
+dezena = número // 10 % 10
+centena = número // 100 % 10
+milhar = número // 1000 % 10
+print('Analisando o número {}'.format(número))
+print('Unidade: {}'.format(unidade))
+print('Dezena: {}'.format(dezena))
+print('Centena: {}'.format(centena))
+print('Milhar: {}'.format(milhar))
