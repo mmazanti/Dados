9662611db653ecbf95576c407f326d829a0698f8
mmazanti <math.mazanti@gmail.com>
2026-04-30 11:45:40 -0300

    Update Desafio025.py
    Ajustado o código para deixar a leitura pro usuário mais intuitiva sobre qual posição a letra 'A' aparece no texto e adicionado o método upper() para fazer a contagem total de letras 'A', independente se são maiúscula sou minúsculas.

--- Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio025.py
+++ Python/Curso_em_video-Mundo 1/pythonDesafios/Desafio025.py
@@ -2,7 +2,7 @@
 #- Quantas vezes aparece a letra "A".
 #- Em que posição ela aparece a primeira vez.
 #- Em que posição ela aparece a última vez.
-frase = input('Digite uma frase: ')
+frase = str(input('Digite uma frase: ')).upper()
 print("A letra 'A' aparece {} vez(es) na frase.".format(frase.count('A')))
-print("A letra 'A' aparece a primeira vez na posição {}.".format(frase.find('A')))
-print("A letra 'A' aparece a última vez na posição {}.".format(frase.rfind('A')))
+print("A letra 'A' aparece a primeira vez na posição {}.".format(frase.find('A')+1))
+print("A letra 'A' aparece a última vez na posição {}.".format(frase.rfind('A')+1))
