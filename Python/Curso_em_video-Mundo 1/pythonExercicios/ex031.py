dfeffd77ed7b432dfbc17fa4b213aae892368a18
mmazanti <math.mazanti@gmail.com>
2026-05-12 18:51:42 -0300

    Create ex031.py
    Resolução do exercício 031: Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$ 0,50 por Km para viagens de até 200Km e R$ 0,45 para viagens mais longas.

---
+++ Python/Curso_em_video-Mundo 1/pythonExercicios/ex031.py
@@ -0,0 +1,10 @@
+#Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$ 0,50 por Km
+#para viagens de até 200Km e R$ 0,45 para viagens mais longas.
+distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {}Km.'.format(distancia))
+'''if distancia <= 200:
+    preço = distancia * 0.50
+else:
+    preço = distancia * 0.45'''
+preço = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print('E o preço da sua passagem será de R${:.2f}'.format(preço))
