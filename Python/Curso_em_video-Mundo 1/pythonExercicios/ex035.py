1403839a32eff389eaeb796916fd461028f09e47
mmazanti <math.mazanti@gmail.com>
2026-05-13 14:17:01 -0300

    Create ex035.py
    Resolução do exercício 035: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.

---
+++ Python/Curso_em_video-Mundo 1/pythonExercicios/ex035.py
@@ -0,0 +1,11 @@
+#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+print('-='*20)
+print('Analisador de Triângulos')
+print('-='*20)
+r1 = float(input('Primeiro seguimento: '))
+r2 = float(input('Segundo seguimento: '))
+r3 = float(input('Terceiro seguimento: '))
+if r1 < r2 + r3 and r2< r1 + r3 and r3 < r1 + r2:
+    print('Os seguimentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os seguimentos acima NÃO PODEM FORMAR triângulo!')
