f7c2b6372811c94bf6deaa09ae72b4d6e204c13e
mmazanti <math.mazanti@gmail.com>
2026-05-19 10:26:08 -0300

    Create ex044.py
    Resolução do exercício 44: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
    -À vista dinheiro/PIX: 10% DE DESCONTO
    -À vista no cartão: 5% DE DESCONTO
    -Em até 2x no cartão: PREÇO NORMAL
    -3x ou mais no cartão: 20% DE JUROS

---
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonExercicios/ex044.py
@@ -0,0 +1,30 @@
+#Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
+#-À vista dinheiro/PIX: 10% DE DESCONTO
+#-À vista no cartão: 5% DE DESCONTO
+#-Em até 2x no cartão: PREÇO NORMAL
+#-3x ou mais no cartão: 20% DE JUROS
+print('{:=^40}'.format(' LOJAS GUANABARA '))
+preço = float(input('Preço das compras: R$'))
+print('''FORMAS DE PAGAMENTO
+[ 1 ] à vista dinheiro/pix
+[ 2 ] à vista cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opção = int(input('Qual é a opção? '))
+if opção == 1:
+    total = preço - (preço * 10 / 100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcela em 2x de R${:.2f}'.format(parcela))
+elif opção == 4:
+    total = preço + (preço * 20 / 100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print('Sua compra será parcelada em {}x de R${:.2f} COM JUROS'.format(totparc, parcela))
+else:
+    total = preço
+    print('OPÇÃO INVÁLIDA de pagamento. Tente novamente!')
+print('Sua compra de R${:.2f} vai custar R${} no final.'.format(preço, total))
