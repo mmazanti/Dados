fdabe9fdaad1913ad9a6804794cde1bb67065a07
mmazanti <math.mazanti@gmail.com>
2026-05-22 10:11:07 -0300

    Update Desafio052.py
    Ajustado a importação de biblioteca trazendo apenas as funções necessárias no código.

--- Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio052.py
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio052.py
@@ -1,9 +1,9 @@
 #Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderendo os espaço.
-import unicodedata
+from unicodedata import normalize, category
 frase = str(input('Digite uma frase: '))
 frase_tratada = frase.lower().replace(" ", "")
-frase_tratada = unicodedata.normalize('NFD', frase_tratada)
-frase_tratada = "".join(ch for ch in frase_tratada if unicodedata.category(ch) != 'Mn')
+frase_tratada = normalize('NFD', frase_tratada)
+frase_tratada = "".join(ch for ch in frase_tratada if category(ch) != 'Mn')
 frase_invertida = frase_tratada[::-1]
 if frase_tratada == frase_invertida:
     print('A frase {} É UM PALÍNDROMO!'.format(frase))
