f0f42f84768ab4de932d036a262b684743268253
mmazanti <math.mazanti@gmail.com>
2026-05-26 12:16:12 -0300

    Update Desafio047.py
    Incluído um contador para melhor entendimento dos acumuladores e contadores e melhorado a lógica do laço for.

--- Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio047.py
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio047.py
@@ -1,6 +1,8 @@
 #Faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 soma = 0
-for contagem in range(1, 501):
-    if contagem % 2 == 1 and contagem % 3 == 0:
+contador = 0
+for contagem in range(1, 501, 2):
+    if contagem % 3 == 0:
         soma += contagem
-print('A soma de todos os números ímpares múltiplos de 3 no range de 1 até 500 é um total de {}.'.format(soma))
+        contador += 1
+print('A soma de todos os {} números ímpares múltiplos de 3 no range de 1 até 500 é um total de {}.'.format(contador, soma))
