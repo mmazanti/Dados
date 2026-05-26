6cc9c11d1c669e308ca06d41f4c58aea8e5e3d69
mmazanti <math.mazanti@gmail.com>
2026-05-26 12:36:23 -0300

    Update Desafio049.py
    Incluído if para trazer resultados melhores quando não foi digitado nenhum valor par e quando se digita apenas um valor par.

--- Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio049.py
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio049.py
@@ -1,7 +1,14 @@
 #Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado por ímpar, desconsidere-o
 soma = 0
-for contagem in range(0, 6):
-    numero = int(input('Digite um número: '))
+contagem = 0
+for laço in range(1, 7):
+    numero = int(input('Digite o {}º número: '.format(laço)))
     if numero % 2 == 0:
         soma += numero
-print('A soma de todos os números pares que você digitou é de {}.'.format(soma))
+        contagem += 1
+if contagem == 1:
+    print('Você digitou {} número PAR e a soma foi de {}'.format(contagem, soma))
+elif contagem > 1:
+    print('Você digitou {} números PARES e a soma foi de {}'.format(contagem, soma))
+else:
+    print('Você digitou nenhum número PAR.')
