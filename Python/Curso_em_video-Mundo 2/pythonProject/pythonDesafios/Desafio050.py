0f5ad0a5181242e898a2a2aec759c9758b57245d
mmazanti <math.mazanti@gmail.com>
2026-05-26 12:53:20 -0300

    Update Desafio050.py
    Simplificando a lógica do décimo_termo e melhorado a saída do programa.

--- Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio050.py
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio050.py
@@ -4,7 +4,7 @@ print('PROGRESSÃO ARITMÉTICA')
 print('=*'*11)
 primeiro_termo = int(input('Digite o primeiro termo da PA: '))
 razão = int(input('Digite a razão da PA: '))
-décimo_termo = primeiro_termo + (9) * razão
-fim_do_range = décimo_termo + razão
-for progressão_aritmética in range(primeiro_termo, fim_do_range, razão):
-    print(progressão_aritmética)
+décimo_termo = primeiro_termo + 10 * razão
+for progressão_aritmética in range(primeiro_termo, décimo_termo, razão):
+    print('{}'.format(progressão_aritmética), end=' ➔ ')
+print('ACABOU!')
