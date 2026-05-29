f2d73402e702980c9a11802d71149efeecde2eab
mmazanti <math.mazanti@gmail.com>
2026-05-28 23:39:57 -0300

    Create Desafio059.py
    Resolução do desafio 59: Faça um programa que leia um número qualquer e mostre o seu fatorial

---
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio059.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número qualquer e mostre o seu fatorial
+from math import factorial
+numero = int(input('Digite um número: '))
+fatorial = factorial(numero)
+print(fatorial)
