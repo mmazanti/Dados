7336ea4c5ebe1b8f5f977c87b880202148706342
mmazanti <math.mazanti@gmail.com>
2026-06-01 13:03:23 -0300

    Create Desafio063.py
    Resolução do desafio 63: Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999 que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando a flag).

---
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio063.py
@@ -0,0 +1,12 @@
+#Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999
+#que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando a flag).
+qtde_número = 0
+número = int(input('Digite um número (para parar digite 999): '))
+menu = número
+soma = 0
+if número != 999:
+    while número != 999:
+        qtde_número += 1
+        soma += número
+        número = int(input('Digite um número (para parar digite 999): '))
+print(f'Você digitou um total de {qtde_número} números e a soma dos números digitados é de {soma}')
