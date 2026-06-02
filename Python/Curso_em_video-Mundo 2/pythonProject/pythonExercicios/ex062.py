6a9abf8b3ac1e632b628115a6f778c7383637dcc
mmazanti <math.mazanti@gmail.com>
2026-06-01 21:48:08 -0300

    Create ex062.py
    Resolução do exercício 62: Melhore o Desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.

---
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonExercicios/ex062.py
@@ -0,0 +1,18 @@
+#Melhore o Desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.
+print('Gerador de PA')
+print('-='*10)
+primeiro = int(input('Primeiro termo: '))
+razão = int(input('Razão de PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total  += mais
+    while cont <= total:
+        print(f'{termo} ➔ ', end='')
+        termo += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print(f'Progressão finalizada com {total} termos mostrados.')
