4a2cda7ff572cb2c54e29772ee98ceb97d51342e
Matheus Mazanti <math.mazanti@gmail.com>
2026-09-21 13:59:56 -0300

    Update Desafio067.py
    Melhorado a lógica e simplificando o código com o parâmetro True e break no comando while.

--- Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio067.py
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio067.py
@@ -1,39 +1,32 @@
 #Faça um programa que jogue para ou impar com o computador. O jogo só será interrompido quando o jogador PERDER, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 from random import randint
 vitórias = 0
-derrota = False
 print('=-' * 20)
 print('VAMOS JOGAR PAR OU ÍMPAR')
-while derrota == False:
+while True:
     print('=-' * 20)
-    computador = randint(0, 10)
-    jogador_número = int(input('Diga um valor: '))
-    jogador_palpite = input('Par ou ìmpar? [P/I] ').upper().strip()
-    par_ou_impar = jogador_número + computador
-    print('-' * 20)
-    if par_ou_impar % 2 == 0 and jogador_palpite == 'P':
-        print(f'Você jogou {jogador_número} e o computador {computador}. Total de {par_ou_impar} DEU PAR.')
-        print('-' * 20)
-        print('Você VENCEU!')
-        vitórias += 1
-        print('Vamos jogar novamente...')
-    elif par_ou_impar % 2 == 0 and jogador_palpite == 'I':
-        print(f'Você jogou {jogador_número} e o computador {computador}. Total de {par_ou_impar} DEU PAR.')
-        print('-' * 20)
-        print('Você PERDEU!')
-        derrota = True
-    elif par_ou_impar % 1 == 0 and jogador_palpite == 'I':
-        print(f'Você jogou {jogador_número} e o computador {computador}. Total de {par_ou_impar} DEU IMPAR.')
-        print('-' * 20)
-        print('Você VENCEU!')
-        vitórias += 1
-        print('Vamos jogar novamente...')
-    elif par_ou_impar % 1 == 0 and jogador_palpite == 'P':
-        print(f'Você jogou {jogador_número} e o computador {computador}. Total de {par_ou_impar} DEU IMPAR.')
-        print('-' * 20)
-        print('Você PERDEU!')
-        derrota = True
-print('=-' * 20)
+    computador = randint(0, 11)
+    jogador_número = int(input('Digite um valor: '))
+    total = jogador_número + computador
+    jogador_palpite = ' '
+    while jogador_palpite not in 'PI':
+        jogador_palpite = str(input('Par ou ìmpar? [P/I] ')).upper().strip()
+    print(f'Você jogou {jogador_número} e o computador {computador}. Total de {total}.')
+    if jogador_palpite == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU!')
+            vitórias += 1
+        else:
+            print('Você PERDEU!')
+            break
+    if jogador_palpite == 'I':
+        if total % 2 == 1:
+            print('Voce VENCEU!')
+            vitórias += 1
+        else:
+            print('Voce PERDEU!')
+            break
+    print('Vamos jogar novamente...')
 if vitórias == 1:
     print(f'GAME OVER! Você venceu {vitórias} vez.')
 elif vitórias > 1:
