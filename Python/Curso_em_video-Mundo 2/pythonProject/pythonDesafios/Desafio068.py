c6b7d2c65343dd147ef6e4a77bee2ce1d4b70b9d
Matheus Mazanti <math.mazanti@gmail.com>
2026-09-21 14:28:45 -0300

    Update Desafio068.py
    Melhorias na lógica utilizando o parâmetro True no comando while.

--- Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio068.py
+++ Python/Curso_em_video-Mundo 2/pythonProject/pythonDesafios/Desafio068.py
@@ -5,9 +5,9 @@
 #C) Quantas mulheres tem menos de 20.
 stop = 'S'
 maior_18 = 0
-homens = 0
-mulheres = 0
-while stop == 'S':
+total_homens = 0
+total_mulheres_abaixo_20 = 0
+while True:
     print('-'*30)
     print(' '*5, 'CADASTRE UMA PESSOA', ' '*5)
     print('-'*30)
@@ -18,9 +18,9 @@ while stop == 'S':
     if idade >= 18:
         maior_18 += 1
     if sexo == 'M':
-        homens += 1
+        total_homens += 1
     if sexo == 'F' and idade < 20:
-        mulheres += 1
+        total_mulheres_abaixo_20 += 1
     stop = input('Quer continuar? [S/N] ').upper().strip()
     while stop != 'S' and stop != 'N':
         stop = input('Quer continuar? [S/N] ').upper().strip()
@@ -28,5 +28,5 @@ while stop == 'S':
         break
 print('='*5, 'FIM DO PROGRAMA', '='*5)
 print(f'Total de pessoas com mais de 18 anos: {maior_18}')
-print(f'Ao todo temos {homens} homens cadastrados.')
-print(f'E temos {mulheres} mulheres com menos de 20 anos.')
+print(f'Total de homens cadastrados: {total_homens}.')
+print(f'Total de mulheres com menos de 20 anos cadastradas: {total_mulheres_abaixo_20}')
